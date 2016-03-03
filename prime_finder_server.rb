d4d3c659b4b9148805ed72b962eec30e7adac8b2
Doug Prostko <dprostko@tripadvisor.com>
2016-03-03 14:48:15 -0500

    set up server / client to not use broatcast

--- prime_finder_server.rb
+++ prime_finder_server.rb
@@ -5,41 +5,42 @@ require 'rinda/tuplespace'
 
 class TupleSpacePrimeFinder
 
-  def self.run
-    DRb.start_service
-    new.run
-  end
-
-  def initialize
-    @ts = Rinda::TupleSpace.new
-  end
-
-  ##
-  # Lists primes as they are added to the TupleSpace.
-
-  def list_primes
-    Thread.start do
-      notifier = @ts.notify 'write', [:prime, nil]
-
-      puts "primes found:"
-      notifier.each do |_, t|
-        puts t.last
-      end
+    def self.run
+        DRb.start_service
+        new.run
     end
-  end
 
-  def run
-    list_primes
+    def initialize
+        #@ts = Rinda::TupleSpace.new
+        @ts = DRbObject.new_with_uri("druby://10.96.232.82:9999")
+    end
 
-    @ts.write [:prime, 2] # seed prime
-    @ts.write [:current, 2] # next value to search
-    @ts.write [:step, 100] # range of values to search
+    ##
+    # Lists primes as they are added to the TupleSpace.
 
-    rp = Rinda::RingProvider.new :TSPF, @ts, 'PrimeFinder using TupleSpace'
-    rp.provide
+    def list_primes
+        Thread.start do
+            notifier = @ts.notify 'write', [:prime, nil]
 
-    DRb.thread.join
-  end
+            puts "primes found:"
+            notifier.each do |_, t|
+                puts t.last
+            end
+        end
+    end
+
+    def run
+        list_primes
+
+        @ts.write [:prime, 2] # seed prime
+        @ts.write [:current, 2] # next value to search
+        @ts.write [:step, 100] # range of values to search
+
+        #rp = Rinda::RingProvider.new :TSPF, @ts, 'PrimeFinder using TupleSpace'
+        #rp.provide
+
+        DRb.thread.join
+    end
 
 end
 
