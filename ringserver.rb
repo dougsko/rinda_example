8e152d34886c94a09a626a56e57ead540ab2f1bf
Doug Prostko <dougtko@gmail.com>
2016-03-03 14:47:48 -0500

    set it up to not use ringserver

--- ringserver.rb
+++ ringserver.rb
@@ -5,10 +5,11 @@
 require 'rinda/ring'
 require 'rinda/tuplespace'
 
-DRb.start_service
+DRb.start_service("druby://10.96.232.82:9999", Rinda::TupleSpace.new)
 
-ts = Rinda::TupleSpace.new
-place = Rinda::RingServer.new(ts)
+#DRb.start_service
+#ts = Rinda::TupleSpace.new
+#place = Rinda::RingServer.new(ts)
 
 DRb.thread.join
 
